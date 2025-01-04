db22fa151634416a45ae05ca1e5040c77f09dacc
GKD3776 <keshav_d@cs.iitr.ac.in>
2025-01-04 15:02:04 +0530

    add audio part

--- scenes/level1.gd
+++ scenes/level1.gd
@@ -1,14 +1,19 @@
 extends Node2D
 
-var villain_scene :PackedScene= load("res://scenes/villian.tscn")
-var rng=RandomNumberGenerator.new()
+var villain_scene : PackedScene = load("res://scenes/villian.tscn")
+var rng = RandomNumberGenerator.new()
 
-func _on_villian_timer_timeout() -> void:
-	var numberofvillians=rng.randi_range(1,4)
-	print(numberofvillians)
-	while numberofvillians:
-		var villain = villain_scene.instantiate()
-		$villians.add_child(villain)
-		numberofvillians-=1
-	$villianTimer.start()
+func _ready() -> void:
+	pass
 	
+func _on_villian_timer_timeout() -> void:
+	var numberofvillians = 1
+	#print("Number of villains:", numberofvillians)
+	
+	while numberofvillians:
+		var villians = villain_scene.instantiate()  # Instantiate the villain
+		$villians.add_child(villians)  # Add the villain to the parent container
+		
+		numberofvillians -= 1
+	
+	$villianTimer.start()
